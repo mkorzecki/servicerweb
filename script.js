b5b54dc300215ec5089bb8ef28ab0d05ae7d3a56
MarKo <personable@o2.pl>
2019-05-14 23:17:19 +0200

    add logic to chose model of laptop

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-function openSeriesFunction() {
-    $(document).ready(function(){
-        $("#container").children("div").children("button").click(function(){
-            $('.seriesClass').hide();
-            $(this).next("div").show();
-        });
-    });
-}
+$(document).ready(function () {
 
-function openForm() {
-    $(document).ready(function(){
-
+    $("#hpButton").click(function () {
+        $('#series').show();
+        $('.seriesClass').hide(2000);
+        $('#seriesHP').show();
     });
-}
+
+    $("#dellButton").click(function () {
+        $('#series').show();
+        $('.seriesClass').hide(2000);
+        $('#seriesDell').show();
+    });
+
+    $("#appleButton").click(function () {
+        $('#series').show();
+        $('.seriesClass').hide(2000);
+        $('#seriesApple').show();
+    });
+
+    $("#sonyButton").click(function () {
+        $('#series').show();
+        $('.seriesClass').hide(2000);
+        $('#seriesSony').show();
+    });
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
+});
+
