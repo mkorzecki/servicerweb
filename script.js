8733f1815a1800ba91f8e6329a21a0eac98a049b
MarKo <personable@o2.pl>
2019-05-15 22:12:27 +0200

    add possibility to chose model for another brand

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ $(document).ready(function () {
     });
 
     $('#exampleModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('whatever') // Extract info from data-* attributes
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var model = button.data('whatever'); // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
+        var modal = $(this);
+        modal.find('.modal-title').text('Zgłoś usterkę dla ' + model);
+        modal.find('.modal-body input').val(model);
       })
 });
 
