d6b069ae794ee5ae25bd98d9229073fa75c7aa23
mkorzecki <personable@o2.pl>
2019-10-25 08:03:18 +0200

    add send JSON from the client level

--- json.js
+++ json.js
@@ -1,43 +1,69 @@
-$(document).ready(function () {
-    $("#sendRequestButton").click(function (e) {
-        var jsonData = {};
+$(document).ready(function() {
+  $("#sendRequestButton").click(function(e) {
+    var jsonData = {};
+    debugger;
+    var formData = $("#requestForm").serializeArray();
+    var subject = formData[10];
+    var description = formData[12];
+    var itemId = { name: "itemid", value: "3" };
+    formData.splice(0, formData.length);
+    formData.push(subject);
+    formData.push(description);
+    formData.push(itemId);
 
-        var formData = $("#requestForm").serializeArray();
-        $.each(formData, function () {
-            if (jsonData[this.name]) {
-                if (!jsonData[this.name].push) {
-                    jsonData[this.name] = [jsonData[this.name]];
-                }
-                jsonData[this.name].push(this.value || '');
-            } else {
-                jsonData[this.name] = this.value || '';
-            }
-        });
-
-        if (isAllFieldsAreFulfiled(formData) == true && validateEmail(jsonData.email) == true) {
-            //    $.ajax(
-            //    {
-            //        url : "action.php",
-            //        type: "POST",
-            //        data : jsonData,
-
-            //    });
-            //e.preventDefault();
+    $.each(formData, function() {
+      if (jsonData[this.name]) {
+        if (!jsonData[this.name].push) {
+          jsonData[this.name] = [jsonData[this.name]];
         }
-        else {
-            console.log("Not all fields was fulfilled")
-        }
+        jsonData[this.name].push(this.value || "");
+      } else {
+        jsonData[this.name] = this.value || "";
+      }
     });
+    debugger;
+    console.log(jsonData);
+    $.ajax({
+      url: "http://localhost:5000/api/tickets",
+      method: "POST",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify({
+        subject: "temattemattemat",
+        description: "opisopisopisopisopisopis",
+        itemid: 3
+      })
+    }).done(response => {
+      console.log(response);
+    });
+    //if (isAllFieldsAreFulfiled(formData) == true /*&& validateEmail(jsonData.email) == true*/) {
+    //     $.ajax({
+    //       url: "http://localhost:5000/api/tickets",
+    //       method: "POST",
+    //       dataType: "json",
+    //       contentType: "application/json",
+    //       data: JSON.stringify({
+    //         subject: "temattemattemat",
+    //         description: "opisopisopisopisopisopis",
+    //         itemid: 3
+    //       })
+    //     }).done(response => {
+    //       console.log(response);
+    //     });
+    // }
+    // else {
+    //     console.log("Not all fields was fulfilled")
+    // }
+  });
 });
 function isAllFieldsAreFulfiled(formData) {
-    var counter = 0;
-    for (i = 0; i < formData.length; i++) {
-        if (formData[i].value != "")
-            counter++;
-    }
-    return counter == formData.length;
+  var counter = 0;
+  for (i = 0; i < formData.length; i++) {
+    if (formData[i].value != "") counter++;
+  }
+  return counter == formData.length;
 }
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#hpButton").click(function() {
+    $("#series").show();
+    $(".seriesClass").hide(2000);
+    $("#seriesHP").show();
+  });
 
-    $("#hpButton").click(function () {
-        $('#series').show();
-        $('.seriesClass').hide(2000);
-        $('#seriesHP').show();
-    });
+  $("#dellButton").click(function() {
+    $("#series").show();
+    $(".seriesClass").hide(2000);
+    $("#seriesDell").show();
+  });
 
-    $("#dellButton").click(function () {
-        $('#series').show();
-        $('.seriesClass').hide(2000);
-        $('#seriesDell').show();
-    });
+  $("#appleButton").click(function() {
+    $("#series").show();
+    $(".seriesClass").hide(2000);
+    $("#seriesApple").show();
+  });
 
-    $("#appleButton").click(function () {
-        $('#series').show();
-        $('.seriesClass').hide(2000);
-        $('#seriesApple').show();
-    });
+  $("#sonyButton").click(function() {
+    $("#series").show();
+    $(".seriesClass").hide(2000);
+    $("#seriesSony").show();
+  });
 
-    $("#sonyButton").click(function () {
-        $('#series').show();
-        $('.seriesClass').hide(2000);
-        $('#seriesSony').show();
-    });
-
-    $('#modalForTicket').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var model = button.data('whatever');
-        var modal = $(this);
-        modal.find('.modal-title').text('Zgłoś usterkę dla ' + model);
-        modal.find('#modelId').val(model);
-        //modal.find('.modal-body input').val(model);
-    });
+  $("#modalForTicket").on("show.bs.modal", function(event) {
+    var button = $(event.relatedTarget);
+    var model = button.data("whatever");
+    var modal = $(this);
+    modal.find(".modal-title").text("Zgłoś usterkę dla " + model);
+    modal.find("#model").val(model);
+    //modal.find('.modal-body input').val(model);
+  });
+//   $("#ticket").click(function() {
+//     $.ajax({
+//       url: "http://localhost:5000/api/tickets",
+//       method: "POST",
+//       dataType: "json",
+//       contentType: "application/json",
+//       data: JSON.stringify({
+//         subject: "temattemattemat",
+//         description: "opisopisopisopisopisopis",
+//         itemid: 3
+//       })
+//     }).done(response => {
+//       console.log(response);
+//     });
+//   });
 });
-
