77e375b7ef44ed8e7b92bc1e77d0d79060b1903a
mkorzecki <personable@o2.pl>
2019-12-20 16:16:01 +0100

    add missing failure type to form

--- json.js
+++ json.js
@@ -10,19 +10,17 @@ $(document).ready(function() {
     var subject = formData[6];
     var description = formData[7];
     var origin = formData[8];
-    var type = formData[9];
-    var status = formData[10];
-    var priority = formData[11];
-    var productionYear = prepareProductionYear(formData);
+    var status = formData[9];
+    var priority = formData[10];
+    var type = formData[12];
+    var productionYear = formData[11].value + "-01-01T00:00:00";
     var manufacturer= formData[13].value;
     var series = formData[14].value;
     var name= formData[15].value;
 
     var person = {firstName, lastName, sex, email, phone};
     var customer = { person: person};
-
-    var productSpecification = {manufacturer, series, name};
-  
+    var productSpecification = {manufacturer, series, name};  
     var item = {name: "item", value: {productionYear, productSpecification, customer}}
 
     formData.splice(0, formData.length);
@@ -59,20 +57,4 @@ $(document).ready(function() {
     }
     });
   });
-
-  function prepareProductionYear (formData) {
-    var d = new Date();
-    var mon = (d.getMonth() + 1).toString();
-    var month = mon.length == 1 ? month = 0 + mon : month = mon;
-    var min = d.getMinutes().toString();
-    var minutes = min.length == 1 ? minutes = 0 + min : minutes = min;
-    var dat = d.getDate().toString();
-    var date = dat.length == 1 ? date = 0 + dat : date = dat;
-    var hou = d.getHours().toString();
-    var hours = hou.length == 1 ? hours = 0 + hou : hours = hou;
-    var sec = d.getSeconds();
-    var seconds = sec.length == 1 ? seconds = 0 + sec : seconds = sec;
-
-    return formData[12].value + "-" + month + "-" + date + "T" + hours + ":" + minutes + ":" + seconds;
-  }
 });
